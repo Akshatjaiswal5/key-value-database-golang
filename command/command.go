1f060390b8ace7ba330fff88ed5c45a2d7c410c7
Akshatjaiswal5 <akshatjaiswal521@gmail.com>
2023-04-13 18:36:23 +0530

    Added QPUSH and QPOP, refactored code.

--- command/command.go
+++ command/command.go
@@ -9,53 +9,66 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// request represents the JSON request sent by the client.
 type request struct {
 	Command string `json:"command"`
 }
 
+// commandHandler handles incoming commands.
 type commandHandler struct {
 	db *datastore.Datastore
 }
 
+// NewCommandHandler creates a new command handler with the given datastore.
 func NewCommandHandler(db *datastore.Datastore) *commandHandler {
 	return &commandHandler{db: db}
 }
 
+// HandleCommand receives and parses incoming commands.
 func (h *commandHandler) HandleCommand(c *gin.Context) {
 	var req request
 	if err := c.ShouldBindJSON(&req); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+
+	// Split the command into parts based on spaces.
 	parts := strings.Split(req.Command, " ")
 	if len(parts) == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid command"})
 		return
 	}
+
+	// Determine which command was sent and call the corresponding method.
 	switch parts[0] {
 	case "SET":
 		h.Set(parts, c)
 	case "GET":
 		h.Get(parts, c)
 	case "QPUSH":
-		h.Set(parts, c)
+		h.QPush(parts, c)
 	case "QPOP":
-		h.Get(parts, c)
+		h.QPop(parts, c)
 	case "BQPOP":
-		h.Set(parts, c)
+		h.BQPop(parts, c)
 	default:
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid command"})
 	}
 }
 
+// Set handles the SET command.
 func (h *commandHandler) Set(parts []string, c *gin.Context) {
 
 	if len(parts) < 3 {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid command"})
 		return
 	}
+
+	// Parse the key and value from the command.
 	key := parts[1]
 	value := parts[2]
+
+	// Parse any optional arguments (expiry time and condition).
 	expirySeconds := 0
 	condition := ""
 	for i := 3; i < len(parts); i++ {
@@ -71,6 +84,7 @@ func (h *commandHandler) Set(parts []string, c *gin.Context) {
 		}
 	}
 
+	// Perform the appropriate operation based on the condition.
 	if condition == "" {
 		h.db.Set(key, value, expirySeconds)
 	} else {
@@ -80,33 +94,78 @@ func (h *commandHandler) Set(parts []string, c *gin.Context) {
 		} else if err != nil && condition == "XX" {
 			h.db.Set(key, value, expirySeconds)
 		}
-
 		c.JSON(http.StatusOK, gin.H{"message": "Not Inserted"})
+		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{"message": "Inserted"})
 }
-func (h *commandHandler) Get(parts []string, c *gin.Context) {
 
+func (h *commandHandler) Get(parts []string, c *gin.Context) {
+	// Ensure that the command is properly formatted
 	if len(parts) != 2 {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid command"})
 		return
 	}
 
+	// Extract the key from the command
 	key := parts[1]
 
+	// Retrieve the timed value from the datastore
 	tValue, err := h.db.Get(key)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "key not found"})
+		// Return an error response if the key is not found in the datastore
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
+	// Return the value associated with the key in the datastore
 	c.JSON(http.StatusOK, gin.H{"value": tValue.Value})
+}
 
-}
+// QPush adds one or more values to a queue with the given key
 func (h *commandHandler) QPush(parts []string, c *gin.Context) {
+	// Ensure the correct number of arguments are provided
+	if len(parts) < 3 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid command"})
+		return
+	}
+	// Get the key and values to be pushed to the queue
+	key := parts[1]
+	values := parts[2:]
+
+	// Call the QPush method on the datastore to push the values to the queue
+	err := h.db.QPush(key, values...)
+	if err != nil {
+		// Return an error response if there was an error while pushing the values
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	// Return a success response
+	c.JSON(http.StatusOK, gin.H{"message": "Pushed"})
 }
+
+// QPop retrieves and removes the top value from the queue with the given key
 func (h *commandHandler) QPop(parts []string, c *gin.Context) {
+	// Ensure the correct number of arguments are provided
+	if len(parts) != 2 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid command"})
+		return
+	}
+	// Get the key of the queue to pop from
+	key := parts[1]
+
+	// Call the QPop method on the datastore to retrieve and remove the top value from the queue
+	value, err := h.db.QPop(key)
+	if err != nil {
+		// Return an error response if the key is not found in the datastore or the queue is empty
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Return the value associated with the key in the datastore
+	c.JSON(http.StatusOK, gin.H{"value": value})
 }
+
 func (h *commandHandler) BQPop(parts []string, c *gin.Context) {
 }
--- datastore/datastore.go
+++ datastore/datastore.go
@@ -6,94 +6,121 @@ import (
 	"time"
 )
 
+// queue represents a simple queue.
 type queue struct {
 	mu     sync.Mutex
 	values []string
 }
 
+// timedValue represents a value that is stored in the datastore
+// along with its expiry time.
 type timedValue struct {
 	mu         sync.Mutex
 	Value      string
 	expiryTime time.Time
 }
 
+// Datastore represents a simple in-memory datastore.
 type Datastore struct {
 	mu     sync.RWMutex
 	values map[string]*timedValue
-	queue  map[string]*queue
+	queues map[string]*queue
 }
 
+// NewDatastore creates a new instance of Datastore.
 func NewDatastore() *Datastore {
 	return &Datastore{
 		values: make(map[string]*timedValue),
-		queue:  make(map[string]*queue),
+		queues: make(map[string]*queue),
 	}
 }
 
+// Set sets the value for the given key in the datastore.
 func (d *Datastore) Set(key string, value string, expirySeconds int) error {
 	d.mu.Lock()
 	defer d.mu.Unlock()
 
 	newTimedValue := &timedValue{
-		Value:      value,
-		expiryTime: time.Date(2099, 12, 31, 23, 59, 59, 999999999, time.UTC),
+		Value: value,
 	}
 
 	if expirySeconds != 0 {
 		newTimedValue.expiryTime = time.Now().Add(time.Duration(expirySeconds) * time.Second)
+	} else {
+		newTimedValue.expiryTime = time.Date(2099, 12, 31, 23, 59, 59, 999999999, time.UTC)
 	}
 
 	d.values[key] = newTimedValue
 	return nil
 }
 
+// Get retrieves the value for the given key from the datastore.
 func (d *Datastore) Get(key string) (*timedValue, error) {
 	d.mu.RLock()
 	defer d.mu.RUnlock()
-	value, ok := d.values[key]
 
+	value, ok := d.values[key]
 	if !ok {
 		return nil, errors.New("key not found")
 	}
+
 	if value.expiryTime.Before(time.Now()) {
 		delete(d.values, key)
 		return nil, errors.New("key not found")
 	}
+
 	return value, nil
 }
 
+// QPush adds the given values to the end of the queue with the given key in the datastore.
+// If the queue does not exist, it is created.
+// This function is thread-safe.
 func (d *Datastore) QPush(key string, values ...string) error {
+	// Acquire a write lock on the datastore to ensure thread safety.
 	d.mu.Lock()
 	defer d.mu.Unlock()
+	// Retrieve the queue with the given key from the datastore.
+	q, ok := d.queues[key]
 
-	q, ok := d.queue[key]
+	// If the queue does not exist, create a new queue and add it to the datastore.
 	if !ok {
 		q = &queue{}
-		d.queue[key] = q
+		d.queues[key] = q
 	}
 
+	// Acquire a write lock on the queue to ensure thread safety.
 	q.mu.Lock()
 	defer q.mu.Unlock()
+
+	// Append the given values to the end of the queue.
 	q.values = append(q.values, values...)
+
 	return nil
 }
 
+// QPop removes and returns the last element of the queue with the given key.
+// If the queue does not exist or is empty, an error is returned.
 func (d *Datastore) QPop(key string) (string, error) {
+	// Lock the mutex to ensure thread safety.
 	d.mu.Lock()
 	defer d.mu.Unlock()
 
-	q, ok := d.queue[key]
+	// Get the queue with the given key.
+	q, ok := d.queues[key]
 	if !ok {
 		return "", errors.New("queue not found")
 	}
 
+	// Lock the queue's mutex to ensure thread safety.
 	q.mu.Lock()
 	defer q.mu.Unlock()
 
+	// If the queue is empty, return an error.
 	if len(q.values) == 0 {
 		return "", errors.New("queue is empty")
 	}
 
+	// Remove and return the last value in the queue.
 	value := q.values[len(q.values)-1]
 	q.values = q.values[:len(q.values)-1]
 	return value, nil
@@ -101,10 +128,10 @@ func (d *Datastore) QPop(key string) (string, error) {
 
 func (d *Datastore) BQPop(key string, timeout time.Duration) (string, error) {
 	d.mu.Lock()
-	q, ok := d.queue[key]
+	q, ok := d.queues[key]
 	if !ok {
 		q = &queue{}
-		d.queue[key] = q
+		d.queues[key] = q
 	}
 	d.mu.Unlock()
 
