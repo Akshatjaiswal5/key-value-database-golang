ac89ccdceedc9625b50e0319a7e12f0cd029b9ed
Akshat Jaiswal <68910240+Akshatjaiswal5@users.noreply.github.com>
2023-04-14 01:31:28 +0530

    Refactoring

--- main.go
+++ main.go
@@ -3,15 +3,22 @@ package main
 import (
 	"key-value-db-golang/command"
 	"key-value-db-golang/datastore"
-
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// Create a new instance of the key-value datastore
 	db := datastore.NewDatastore()
+
+	// Create a new command handler and pass in the datastore
 	cmdHandler := command.NewCommandHandler(db)
+
+	// Create a new Gin router
 	router := gin.Default()
 
+	// Map the command handler to the root path for incoming POST requests
 	router.POST("/", cmdHandler.HandleCommand)
+
+	// Start the web server and listen for incoming HTTP requests on port 8080
 	router.Run(":8080")
 }
