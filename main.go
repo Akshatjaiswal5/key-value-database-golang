f9d5dfddf5cc754f90af73dce83711c7c7e6ca4f
Akshatjaiswal5 <akshatjaiswal521@gmail.com>
2023-04-14 13:05:41 +0530

    -

--- main.go
+++ main.go
@@ -3,16 +3,24 @@ package main
 import (
 	"key-value-db-golang/command"
 	"key-value-db-golang/datastore"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
+var router *gin.Engine
+
 func main() {
 	db := datastore.NewDatastore()
 	cmdHandler := command.NewCommandHandler(db)
-	router := gin.Default()
+	router = gin.Default()
 
 	router.POST("/", cmdHandler.Handler)
 	router.Run(":" + os.Getenv("PORT"))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	router.ServeHTTP(w, r)
+
 }
